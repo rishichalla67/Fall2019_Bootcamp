56a43a51da9c491bf48e714d45c60b73cdc307b8
rishichalla67 <rishichalla67@gmail.com>
2019-09-17 14:36:33 -0400

    Submission

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    db: {
+        uri: 'mongodb+srv://rchalla:rchalla67@cluster0-tsohw.mongodb.net/test?retryWrites=true&w=majority',
+    }
+};
--- queries.js
+++ queries.js
@@ -1,36 +1,46 @@
 /* Add all the required libraries*/
+//var fs = require('fs'),
+    mongoose = require('mongoose');
+    Schema = mongoose.Schema;
+    Listing = require('./ListingSchema.js');
+    config = require('./config.js');
 
 /* Connect to your database using mongoose - remember to keep your key secret*/
-
+mongoose.connect(config.db.uri, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
 /* Fill out these functions using Mongoose queries*/
 //Check out - https://mongoosejs.com/docs/queries.html
+//mongoose.set('useFindAndModify', false);
 
 var findLibraryWest = function() {
-  /* 
-    Find the document that contains data corresponding to Library West,
-    then log it to the console. 
-   */
+    //findOne function
+    Listing.findOne({ 'name': 'Library West' }, function (err, data) {
+        if (err) {
+            return handleError(err);
+        }
+        console.log(data);
+    });
+
 };
 var removeCable = function() {
-  /*
-    Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed 
-    on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
-    and remove this listing from your database and log the document to the console. 
-   */
+    Listing.findOneAndRemove({ 'code': 'CABL' }, function (err, data) {
+        if (err){
+            return handleError(err);
+        }
+        console.log(data);
+    });
 };
-var updatePhelpsLab = function() {
-  /*
-    Phelps Lab address is incorrect. Find the listing, update it, and then 
-    log the updated document to the console. 
-    
-    Correct Address: 1953 Museum Rd, Gainesville, FL 32603
-
-   */
+var updatePhelpsMemorial = function() {
+    //I hope this is the correct address we are supposed to use.
+    Listing.findOneAndUpdate({'name': 'Phelps Laboratory'}, { 'address': '1953 Museum Rd, Gainesville, FL 32603' }, function (err, data) {
+    });
+    Listing.findOne({ 'name': 'Phelps Laboratory'}, function (err, data) {
+    });
 };
 var retrieveAllListings = function() {
-  /* 
-    Retrieve all listings in the database, and log them to the console. 
-   */
+    //empty brackets mean all listings
+    Listing.find({},function(err,data){
+        console.log(data);
+    });
 };
 
 findLibraryWest();
